ed7c8d0c6a87312dd551d91701c93764898a1787
Luong Hieu Thi <luonghieuthi@gmail.com>
2021-11-12 21:04:05 +0900

    Fully working

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import joplin from 'api';
+import { ContentScriptType } from "api/types";
 
 joplin.plugins.register({
 	onStart: async function() {
-		console.info('Hello world. Test plugin started!');
+		await joplin.contentScripts.register(
+			ContentScriptType.MarkdownItPlugin,
+			'copyAnchorLink',
+			'./copyAnchorLink.js'
+		);
+
+		await joplin.contentScripts.onMessage('copyAnchorLink', async (message:any) => {
+			const selectedNote = await joplin.workspace.selectedNote();
+			const id = message.split(':')[1];
+			joplin.clipboard.writeText(`[${id}](:/${selectedNote.id}\#${id})`);
+		});
 	},
 });
